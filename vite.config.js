dbacef2c9c88602a894c3a3f5ac7a5da492f054d
codewarrior4 <mayowatope119@gmail.com>
2026-09-09 08:46:42 +0100

    fix: Update API_BASE to use environment variable for development mode and configure server proxy settings

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/BurnerX/',
   plugins: [
     vue(),
-    vueDevTools(),
-    tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.mail.tm',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
